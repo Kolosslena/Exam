35049f0c01b7d0c371a21128356b78c768433f08
Kolosslena <60133772+Kolosslena@users.noreply.github.com>
2020-04-25 09:01:41 +0400

    Создание класса с данными

---
+++ src/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src
+{
+    class Person
+    {
+        string fam =" ";
+        string name = " ";
+        int age = 0;
+    }
+}
