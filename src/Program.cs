76178fad66df67e281f39da3e6f6a3422af14e13
Kolosslena <60133772+Kolosslena@users.noreply.github.com>
2020-04-25 11:00:11 +0400

    Данные

--- src/Program.cs
+++ src/Program.cs
@@ -8,25 +8,69 @@ namespace src
 {
     class Program
     {
+        private static int A = 0;
+        private static Person[] People;
         static void Main(string[] args)
         {
-            Console.Write("Введите кол-во людей:");
-            var PeopleCount = Convert.ToInt32(Console.ReadLine());
-            Console.Write("\n");
-            Person[] Person = new Person[PeopleCount];
-            for (int i = 0; i >= PeopleCount; i++)
+            A = Convert.ToInt32(Console.ReadLine());
+            People = new Person[A];
+        }
+        private static void ReadA()
+        {
+            Console.WriteLine("Введите целочисленный размер массива:");
+            string read = Console.ReadLine();
+            while (!Int32.TryParse(read, out A))
             {
-                Console.WriteLine("Введите имя:");
-                Person[i].Name = Console.ReadLine();
+                Console.WriteLine("Некорректный ввод");
+                read = Console.ReadLine();
+            }
+        }
+        private static void FillPeople()
+        {
+            for (int i = 0; i < People.Length; i++)
+            {
+                Console.WriteLine("Введите" + (i + 1) + "й элемент массива");
+                People[i] = new Person();
                 Console.WriteLine("Введите фамилию:");
-                Person[i].Fam = Console.ReadLine();
-                Console.WriteLine("Введите кол-во лет:");
-                var count = Console.ReadLine();
-                if (string.IsNullOrWhiteSpace(count))
+                string fam = Console.ReadLine();
+                while (HasStrDigits(fam))
                 {
+                    Console.WriteLine("Фамилия не может содержать цифр");
+                    fam = Console.ReadLine();
+                }
+                People[i].Fam = fam;
 
+                Console.WriteLine("Введите имя:");
+                string name = Console.ReadLine();
+                while (HasStrDigits(name))
+                {
+                    Console.WriteLine("Имя не может содержать цифр");
+                    name = Console.ReadLine();
+                }
+                People[i].Name = name;
+
+                string read = Console.ReadLine();
+                bool Read = read.AsEnumerable().Any(ch => char.IsLetter(ch));
+                if (!Read)
+                    People[i].Age = Convert.ToInt32(read);
+                else
+                {
+                    Console.WriteLine("Год не должен содержать буквы")
+                }
+                while (Read) ;
+            }
+        }
+        private static bool HasStrDigits(string str)
+        {
+            bool resp = false;
+            for(int i=0; i < str.Length; i++)
+            {
+                if (str[i] >= '0' && str[i] <= '9')
+                {
+                    resp = true;
                 }
             }
+            return resp;
         }
     }
 }
